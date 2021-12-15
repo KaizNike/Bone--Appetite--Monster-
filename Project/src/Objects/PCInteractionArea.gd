145883af5ba6ab266efd631c44b28c50f002b92c
KaizNike <62921588+KaizNike@users.noreply.github.com>
2021-12-14 18:19:50 -0600

    The player sees the pot, the pot sees the player
    Love doing little jaunts into the code!

--- Project/src/Objects/PCInteractionArea.gd
+++ Project/src/Objects/PCInteractionArea.gd
@@ -1,4 +1,10 @@
 extends Area
 
 var interact_priority = 1
-var targetted = false
+var targetted := false setget targetted_changed
+
+signal change_target_status(Bool)
+
+func targetted_changed(new_val):
+	emit_signal("change_target_status", new_val)
+	targetted = new_val
--- Project/src/Objects/PotCook.gd
+++ Project/src/Objects/PotCook.gd
@@ -9,9 +9,16 @@ extends StaticBody
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("food cooking")
+	$PCInteractionArea.connect("change_target_status",self,"target_status_changed")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func target_status_changed(Bool):
+	if Bool:
+		$Indicator.visible = true
+	else:
+		$Indicator.visible = false
